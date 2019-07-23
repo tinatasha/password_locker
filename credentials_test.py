bbb75b5ef1a6534b644a7bae27918522800027ae
tinatasha <50870172+tinatasha@users.noreply.github.com>
2019-07-23 09:28:29 +0300

    modified changes

--- credentials_test.py
+++ credentials_test.py
@@ -9,7 +9,7 @@ class TestCredentials(unittest.TestCase):
         """
         Setup method that defines instructions
         """
-        self.new_credentials = Credentials("Github","Kibet1816","@#soccerkibe1816")
+        self.new_credentials = Credentials("Github","tinatasha","blackfaff")
 
     def tearDown(self):
         """
@@ -22,8 +22,8 @@ def test_init(self):
         Test for correct initialization
         """
         self.assertEqual(self.new_credentials.account_name,"Github")
-        self.assertEqual(self.new_credentials.username,"Kibet1816")
-        self.assertEqual(self.new_credentials.password,"@#soccerkibe1816")
+        self.assertEqual(self.new_credentials.username,"tinatasha")
+        self.assertEqual(self.new_credentials.password,"blackfaffp1")
 
     def test_save_credentials(self):
         """
